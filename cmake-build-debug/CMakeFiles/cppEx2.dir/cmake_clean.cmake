a3da86eb7083b8c5237634a2cf86b90de578cb9f
ZaharAd <zahar91@gmail.com>
2017-01-02 10:45:30 +0200

    start

---
+++ cmake-build-debug/CMakeFiles/cppEx2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppEx2.dir/main.cpp.o"
+  "CMakeFiles/cppEx2.dir/MemPool.cpp.o"
+  "cppEx2.pdb"
+  "cppEx2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppEx2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
